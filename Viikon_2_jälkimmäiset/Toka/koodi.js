c1ef63cf5f465422ff66bc8f4e8eaf152d3897e5
SuloForsgren <sulo.forsgren@gmail.com>
2021-04-12 09:08:51 +0300

    Korjattu / 12.4.2021

---
+++ Viikon_2_jälkimmäiset/Toka/koodi.js
@@ -0,0 +1,27 @@
+function munfunktio() {
+var x = document.getElementById("luku").value;
+  if (x == 1) {
+    alert("Maanantai");
+  }
+    else if (x == 2) {
+    alert("Tiistai");
+  }
+      else if (x == 3) {
+    alert("Keskiviikko");
+  }
+        else if (x == 4) {
+    alert("Torstai");
+  }
+          else if (x == 5) {
+    alert("Perjantai");
+  }
+            else if (x == 6) {
+    alert("Lauantai");
+  }
+  else if (x == 7) {
+    alert("Sunnuntai");
+  }
+  else {
+    alert("");
+  }
+}
