d3c6aed8f8f47e73d01db8dda7c9af4e85b5fdc4
Koodiapina <78953715+SuloForsgren@users.noreply.github.com>
2021-04-12 10:34:11 +0300

    Delete Viikon 2 jälkimmäiset directory
    poistettu turhat

--- Viikon 2 jälkimmäiset/Eka/koodideemus.js
+++
@@ -1,7 +0,0 @@
-function munfunktio(luku) {
-  if (luku <= 0)
-  alert ("negatiivinen");
-  else {
-    alert ("positiivinen");
-  }
-}
--- Viikon 2 jälkimmäiset/Viides/koodi.js
+++
@@ -1,9 +0,0 @@
-function munfunktio() {
-var kluku = parseInt(document.getElementById('kerto').value);
-var lausemuuttuja = "";
-for (var k=1;k<=10;k++)
-{
-  lausemuuttuja += kluku + 'x' + k +' = ' + kluku * k + ' /n';
-  }
-  document.getElementById('vastaus5').innerHTML = "<pre>" + lausemuuttuja + "</pre>"
-}
